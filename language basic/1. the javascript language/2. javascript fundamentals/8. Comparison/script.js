58e278dcef4742344dbd16d80b2062ad60c90d32
Santosh Mahto <mahtos480@gmail.com>
2019-05-04 17:58:44 +0530

    project restructured

---
+++ language basic/1. the javascript language/2. javascript fundamentals/8. Comparison/script.js
@@ -0,0 +1,19 @@
+/**
+ * @author Santosh Mahto
+ * @version 1.0.1
+ * @todo Comparisons practices
+ * @see learning and practices Comparisons
+ */
+"use strict";
+console.log( 'Z' > 'A' ); // true
+console.log( 'Glow' > 'Glee' ); // true
+console.log( 'Bee' > 'Be' ); // tru
+
+// Strict equality
+console.log( 0 == false ); // true
+console.log( '' == false ); // true
+
+
+console.log( undefined > 0 ); // false (1)
+console.log( undefined < 0 ); // false (2)
+console.log( undefined == 0 ); // false (3)
---
+++ language basic/1. the javascript language/2. javascript fundamentals/9. interaction/script.js
@@ -0,0 +1,7 @@
+/**
+ * @author Santosh Mahto
+ * @version 1.0.1
+ * @todo Interaction practices
+ * @see learning and practices Interaction
+ */
+"use strict";
