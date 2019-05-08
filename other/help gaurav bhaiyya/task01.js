ad3d6ebb09dd15ece272419ad55ae2dcb402503c
Santosh Mahto <mahtos480@gmail.com>
2019-05-08 09:02:50 +0530

    email id validation with regular exprestions

--- other/help gaurav bhaiyya/task01.js
+++ other/help gaurav bhaiyya/task01.js
@@ -1,92 +1,100 @@
 
-      function validate() {
-        "use strict";
-        let flag,
-          flagFirstName,
-          flagLastName,
-          flagContact,
-          flagEmailId,
-          flagAddress;
+function validate() {
+  "use strict";
+  let flag,
+    flagFirstName,
+    flagLastName,
+    flagContact,
+    flagEmailId,
+    flagAddress;
 
-        let eleFirstName = document.querySelector("#first_name");
-        let errFirstName = document.querySelector("#error_first_name");
-        let firstName = eleFirstName.value;
-        // console.log("TCL: validate -> firstName", firstName);
+  // regular expressions
+  // const REG_EMAIL = /^[a-zA-Z0-9._-]+[a-zA-Z0-9.-]+\.[a-zA-Z]{2,6}$/;
+  const REG_EMAIL = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-z]{2,6}$/;
 
-        let eleLastName = document.querySelector("#last_name");
-        let errLastName = document.querySelector("#error_last_name");
-        let lastName = eleLastName.value;
-        // console.log("TCL: validate -> lastName", lastName);
+  let eleFirstName = document.querySelector("#first_name");
+  let errFirstName = document.querySelector("#error_first_name");
+  let firstName = eleFirstName.value;
+  // console.log("TCL: validate -> firstName", firstName);
 
-        let eleEmailId = document.querySelector("#email_id");
-        let errEmailId = document.querySelector("#error_email_id");
-        let emailId = eleEmailId.value;
-        // console.log("TCL: validate -> emailId", emailId);
+  let eleLastName = document.querySelector("#last_name");
+  let errLastName = document.querySelector("#error_last_name");
+  let lastName = eleLastName.value;
+  // console.log("TCL: validate -> lastName", lastName);
 
-        let eleContact = document.querySelector("#contact");
-        let errContact = document.querySelector("#error_contact");
-        let contact = eleContact.value;
-        // console.log("TCL: validate -> contact", contact);
+  let eleEmailId = document.querySelector("#email_id");
+  let errEmailId = document.querySelector("#error_email_id");
+  let emailId = eleEmailId.value;
+  // console.log("TCL: validate -> emailId", emailId);
 
-        let eleAddress = document.querySelector("#address");
-        let errAddress = document.querySelector("#error_address");
-        let address = eleAddress.value;
-        // console.log("TCL: validate -> address", address);
+  let eleContact = document.querySelector("#contact");
+  let errContact = document.querySelector("#error_contact");
+  let contact = eleContact.value;
+  // console.log("TCL: validate -> contact", contact);
 
-        if (firstName === "") {
-          flagFirstName = false;
-          errFirstName.innerHTML = "Please Enter The first_name !";
-          errFirstName.style.color = "red";
-        } else {
-          flagFirstName = true;
-          errFirstName.innerHTML = "";
-        }
+  let eleAddress = document.querySelector("#address");
+  let errAddress = document.querySelector("#error_address");
+  let address = eleAddress.value;
+  // console.log("TCL: validate -> address", address);
 
-        if (lastName === "") {
-          flagLastName = false;
-          errLastName.innerHTML = "Please Enter The last Name !";
-          errLastName.style.color = "red";
-        } else {
-          flagLastName = true;
-          errLastName.innerHTML = "";
-        }
+  if (firstName === "") {
+    flagFirstName = false;
+    errFirstName.innerHTML = "Please Enter The first_name !";
+    errFirstName.style.color = "red";
+  } else {
+    flagFirstName = true;
+    errFirstName.innerHTML = "";
+  }
 
-        if (emailId === "") {
-          flagEmailId = false;
-          errEmailId.innerHTML = "Please Enter The Email_id !";
-          errEmailId.style.color = "red";
-        } else {
-          flagEmailId = true;
-          errEmailId.innerHTML = "";
-        }
+  if (lastName === "") {
+    flagLastName = false;
+    errLastName.innerHTML = "Please Enter The last Name !";
+    errLastName.style.color = "red";
+  } else {
+    flagLastName = true;
+    errLastName.innerHTML = "";
+  }
 
-        if (contact === "") {
-          errContact.innerHTML = "Please Enter The contact";
-          errContact.style.color = "red";
-          flagContact = false;
-        } else {
-          flagContact = true;
-          errContact.innerHTML = "";
-        }
+  if (emailId === "") {
+    flagEmailId = false;
+    errEmailId.innerHTML = "Please Enter The Email_id !";
+    errEmailId.style.color = "red";
+  // } else if (true) {
+  //   flagEmailId = false;
+  //   errEmailId.innerHTML = "Invalid Email id !, Please try again.";
+  //   errEmailId.style.color = "red";
+  } else {
+    flagEmailId = true;
+    errEmailId.innerHTML = "";
+    alert(REG_EMAIL.test(emailId));
+  }
 
-        if (address === "") {
-          flagAddress = false;
-          errAddress.innerHTML = "Please Enter The address !";
-          errAddress.style.color = "red";
-        } else {
-          flagAddress = true;
-          errAddress.innerHTML = "";
-        }
+  if (contact === "") {
+    errContact.innerHTML = "Please Enter The contact";
+    errContact.style.color = "red";
+    flagContact = false;
+  } else {
+    flagContact = true;
+    errContact.innerHTML = "";
+  }
 
-        flag = flagFirstName && flagLastName && flagContact && flagEmailId && flagAddress ? true : false;
-        return flag;
-        
-        // return flag = flagFirstName && flagLastName && flagContact && flagEmailId && flagAddress ? true : false;
-        // console.log("TCL: validate -> flag", flag);
-        // console.log("TCL: validate -> flagAddress", flagAddress);
-        // console.log("TCL: validate -> flagEmailId", flagEmailId);
-        // console.log("TCL: validate -> flagContact", flagContact);
-        // console.log("TCL: validate -> flagLastName", flagLastName);
-        // console.log("TCL: validate -> flagFirstName", flagFirstName);
-    }
-    
+  if (address === "") {
+    flagAddress = false;
+    errAddress.innerHTML = "Please Enter The address !";
+    errAddress.style.color = "red";
+  } else {
+    flagAddress = true;
+    errAddress.innerHTML = "";
+  }
+
+  flag = flagFirstName && flagLastName && flagContact && flagEmailId && flagAddress ? true : false;
+  return flag;
+
+  // return flag = flagFirstName && flagLastName && flagContact && flagEmailId && flagAddress ? true : false;
+  // console.log("TCL: validate -> flag", flag);
+  // console.log("TCL: validate -> flagAddress", flagAddress);
+  // console.log("TCL: validate -> flagEmailId", flagEmailId);
+  // console.log("TCL: validate -> flagContact", flagContact);
+  // console.log("TCL: validate -> flagLastName", flagLastName);
+  // console.log("TCL: validate -> flagFirstName", flagFirstName);
+}
