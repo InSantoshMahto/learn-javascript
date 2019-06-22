6c7357be757da44efb2285436edc6f264eaf36a8
Santosh Mahto <mahtos480@gmail.com>
2019-06-22 16:03:42 +0530

    learn classes and inheritance

--- typescript/app.js
+++ typescript/app.js
@@ -1,2 +1,52 @@
 "use strict";
-console.log('hello world');
+var __extends = (this && this.__extends) || (function () {
+    var extendStatics = function (d, b) {
+        extendStatics = Object.setPrototypeOf ||
+            ({ __proto__: [] } instanceof Array && function (d, b) { d.__proto__ = b; }) ||
+            function (d, b) { for (var p in b) if (b.hasOwnProperty(p)) d[p] = b[p]; };
+        return extendStatics(d, b);
+    };
+    return function (d, b) {
+        extendStatics(d, b);
+        function __() { this.constructor = d; }
+        d.prototype = b === null ? Object.create(b) : (__.prototype = b.prototype, new __());
+    };
+})();
+/**
+ * @descriptions
+ * classes
+ */
+var Person = /** @class */ (function () {
+    function Person(name, username) {
+        this.username = username;
+        this.type = 'default';
+        this.age = 21;
+        this.name = name;
+    }
+    Person.prototype.printAge = function () {
+        console.info(this.age);
+        this.setType("love");
+    };
+    Person.prototype.setType = function (type) {
+        this.type = type;
+        console.info(this.type);
+    };
+    return Person;
+}());
+var person = new Person("Santosh", "sks");
+console.info(person);
+console.info(person.name, person.username);
+person.printAge();
+// person.setType(`india`)
+// inheritance
+var Santosh = /** @class */ (function (_super) {
+    __extends(Santosh, _super);
+    function Santosh() {
+        var _this = _super !== null && _super.apply(this, arguments) || this;
+        _this.state = 'jharkhand';
+        return _this;
+    }
+    return Santosh;
+}(Person));
+var santosh = new Santosh('india', 'IN');
+console.info(santosh);
--- typescript/app.ts
+++ typescript/app.ts
@@ -1,1 +1,38 @@
-console.log('hello world')
+/**
+ * @descriptions
+ * classes
+ */
+class Person {
+    public name: string;
+    private type: string = 'default';
+    protected age: number = 21;
+
+    constructor(name: string, public username: string) {
+        this.name = name;
+    }
+
+    public printAge(): void {
+        console.info(this.age);
+        this.setType(`love`);
+    }
+
+    private setType(type: string): void {
+        this.type = type;
+        console.info(this.type);
+    }
+}
+
+const person = new Person("Santosh", "sks");
+console.info(person);
+console.info(person.name, person.username);
+person.printAge();
+// person.setType(`india`)
+
+
+// inheritance
+class Santosh extends Person {
+    state: string = 'jharkhand';
+}
+
+const santosh = new Santosh('india', 'IN');
+console.info(santosh);
