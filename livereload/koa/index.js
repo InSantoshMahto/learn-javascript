6c4831d9f9a9ee8ab3a6aa29149a42ee9ed074e7
Santosh Mahto <mahtos480@gmail.com>
2019-07-12 04:47:17 +0530

    succesfully tested koa-liverefresh

--- livereload/koa/index.js
+++ livereload/koa/index.js
@@ -4,24 +4,26 @@ const cors = require('@koa/cors');
 const static = require('koa-static');
 const render = require('koa-ejs');
 const path = require('path');
+const livereload = require('koa-liverefresh')
 
-const app = (module.exports = new Koa());
+const app = new Koa();
 const router = new Router();
-
-let livereload = require('./lib');
 
 // importing views
 const INDEX = 'index';
+// set port
+const PORT = process.env.PORT || 80;
 
 // allow cors request
 app.use(cors());
+
 // serve static file as a public
 app.use(static(path.join(__dirname, '/public')));
 
 // view engine with ejs rendering
 render(app, {
   root: path.join(__dirname, 'views'),
-  // layout: 'layout',
+  layout: 'layout',
   viewExt: 'ejs',
   delimiter: '%',
   cache: false,
@@ -29,29 +31,27 @@ render(app, {
   async: true,
 });
 
+/**
+ * @description configure koa-liverefresh
+ */
+livereload(
+  router,
+  path.join(__dirname, 'public'),
+  path.join(__dirname, 'views')
+);
+
+// root get request handler
 router.get('/', async (ctx) => {
-  // ctx.body = `<h1 style='text-align:center'>hello world</h1>`;
   let sse = ``;
-  await ctx.render(INDEX,{
-    sse
+  await ctx.render(INDEX, {
+    sse,
   });
-});
-
-router.get('/test', (ctx) => {
-  ctx.body = `<h1>i am working</h1>`;
-});
-
-livereload(router);
-
-// ping routes for check whether server is alive or not
-router.get('/ping', (ctx) => {
-  ctx.body = `i am alive`;
 });
 
 // attached router object with app
 app.use(router.routes()).use(router.allowedMethods());
 
-if (!module.parent)
-  app.listen(80, () => {
-    console.log('server is listening port: 80');
-  });
+// listen to the server
+app.listen(PORT, () => {
+  console.log(`server is listening port: ${PORT}`);
+});
