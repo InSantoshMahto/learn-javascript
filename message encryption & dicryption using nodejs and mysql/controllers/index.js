fcb1f12c773565cb8baec7548c9c7c891e16ff38
Santosh Mahto <mahtos480@gmail.com>
2019-12-05 18:29:50 +0530

    encryprion using mysql tested

--- message encryption & dicryption using nodejs and mysql/controllers/index.js
+++
@@ -1,4 +0,0 @@
-module.exports = {
-	midlewares: require('./midlewares'),
-	utils: require('./utils'),
-};
--- message encryption & dicryption using nodejs and mysql/controllers/midlewares/index.js
+++
@@ -1,5 +0,0 @@
-const appMidlewares = require('./app.midleware');
-module.exports = {
-  errorhandler: appMidlewares.errorhandler,
-  authenticator: appMidlewares.authenticator
-}
