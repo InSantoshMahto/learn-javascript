41e0a0829d757aa4f7a3bc6069107c5292a57441
santosh mahto <mahtos480@gmail.com>
2020-03-05 04:07:39 +0530

    practice ssr with nest js

---
+++ angular/ssr-nest/server/app.module.ts
@@ -0,0 +1,18 @@
+import { Module } from '@nestjs/common';
+import { AngularUniversalModule } from '@nestjs/ng-universal';
+import { join } from 'path';
+import { AppServerModule } from '../src/main.server';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+
+@Module({
+  imports: [
+    AngularUniversalModule.forRoot({
+      bootstrap: AppServerModule,
+      viewsPath: join(process.cwd(), 'dist/ssr-nest/browser')
+    })
+  ],
+  controllers: [AppController],
+  providers: [AppService]
+})
+export class AppModule {}
