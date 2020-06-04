d55b01648da3778c9e56fe73ab72bc9794ea0cef
santosh mahto <mahtos480@gmail.com>
2020-06-04 09:28:55 +0530

    learn react state

---
+++ react/penguin/src/components/Clock.js
@@ -0,0 +1,31 @@
+import React from 'react';
+
+export class Clock extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { date: new Date() };
+  }
+
+  componentDidMount() {
+    this.timerId = setInterval(() => this.tick(), 1000);
+  }
+
+  componentWillUnmount() {
+    clearInterval(this.timerId);
+  }
+
+  tick() {
+    this.setState({
+      date: new Date(),
+    });
+  }
+
+  render() {
+    return (
+      <div>
+        <h1>Hello, world!</h1>
+        <h2>It is {this.state.date.toLocaleTimeString()}.</h2>
+      </div>
+    );
+  }
+}
