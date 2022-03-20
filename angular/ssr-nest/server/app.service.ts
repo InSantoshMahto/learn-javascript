eae87609b742975b3c5815bdbe0a5aeaea5ef7bb
SANTOSH MAHTO <mahtos480@gmail.com>
2022-03-20 17:21:39 +0530

    remove outdated things from angular

--- angular/ssr-nest/server/app.service.ts
+++
@@ -1,8 +0,0 @@
-import { Injectable } from '@nestjs/common';
-
-@Injectable()
-export class AppService {
-  getRoot(): { success: boolean; [key: string]: any } {
-    return { success: true, data: 'hello world' };
-  }
-}
